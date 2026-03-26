651c3975da807d04e40fb2b01ed694d6ce998b76
Durbek2011 <durbekmamademinov@gmail.com>
2026-03-26 23:28:04 +0500

    Main created

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
       </Routes>
+      
     </div>
   )
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import Banner from "./Banner"
-import Header from "./Header"
 import Main from "./Main"
 
 function Home() {
